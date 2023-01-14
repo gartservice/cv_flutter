97d52458ca56afcc3bd9e08365fdb8744f4de748
gartservice <gart.service@yandex.ru>
2023-01-15 07:57:45 +0800

    Text my container

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,54 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child:Column(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              CircleAvatar(
-                child: Image(image: AssetImage('/images/avatar.jpg')),
-              ),
-              Container(
-                width: 100.0,
-                color: Colors.red,
-              ),
+              Column(
+                children: [
+                  SizedBox(
+                    height: 150.0,
+                  ),
+                  Container(
+                    child: CircleAvatar(
+                      maxRadius: 100.0,
+                      backgroundImage: AssetImage('images/avatar.jpg'),
+                    ),
+                  ),
+                  Container(
+                    child: Text(
+                      'Artem Garanskyy',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontFamily: 'BebasNeue',
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Text(
+                      'Flutter developer',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'SourceCodePro',
+                        color: Colors.black54,
+                        fontWeight: FontWeight.w900,
+                        letterSpacing: 5,
+
+                      ),
+                    ),
+                  ),
+
+                  Container(
+                    child: Text('text'),
+                    width: 400,
+                    height: 100.0,
+                    color: Colors.white,
+
+                  ),
+                ],
+              )
             ],
           ),
         ),
