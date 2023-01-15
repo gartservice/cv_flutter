4f1d218dadc9910d7027477244b0560716b524a7
gartservice <gart.service@yandex.ru>
2023-01-15 10:43:41 +0800

    Finish app

--- lib/main.dart
+++ lib/main.dart
@@ -13,21 +13,22 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 maxRadius: 100.0,
                 backgroundImage: AssetImage('images/avatar.jpg'),
               ),
-              SizedBox(height: 15),
+              SizedBox(
+                height: 15,
+              ),
               Text(
-
                 'Artem Garanskyy',
                 style: TextStyle(
-                  fontSize: 25,
-                  fontFamily: 'BebasNeue',
-                  color: Colors.white,
-                  letterSpacing: 2.5
-                ),
+                    fontSize: 25,
+                    fontFamily: 'BebasNeue',
+                    color: Colors.white,
+                    letterSpacing: 2.5),
               ),
               Text(
                 'Flutter developer',
@@ -39,56 +40,46 @@ class MyApp extends StatelessWidget {
                   letterSpacing: 5,
                 ),
               ),
+              SizedBox(
+                height: 15,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
               Card(
-                  color: Colors.white,
-                  margin:
-                      EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.phone,
-                          size: 25,
-                          color: Colors.teal.shade900,
-                        ),
-                        SizedBox(
-                          width: 12,
-                        ),
-                        Text(
-                          '+61 415 555 555',
-                          style: TextStyle(
-                            color: Colors.teal.shade900,
-                            fontFamily: 'PlayfairDisplay',
-                            fontSize: 20,
-                          ),
-                        ),
-                      ],
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    '+61 415 555 555',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'PlayfairDisplay',
+                      fontSize: 20,
                     ),
-                  )),
+                  ),
+                ),
+              ),
               Card(
                 margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
                 color: Colors.white,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-
-                  children: [
-                    Icon(Icons.mail,size: 20, color: Colors.teal.shade900),
-                    SizedBox(width: 12),
-                    Text('artem.dev@gmail.com',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: 'PlayfairDisplay',
-                        color: Colors.teal.shade900,
-                      ),
+                child: ListTile(
+                  leading: Icon(Icons.mail, color: Colors.teal.shade900),
+                  title: Text(
+                    'artem.dev@gmail.com',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'PlayfairDisplay',
+                      color: Colors.teal.shade900,
                     ),
-                  ],
+                  ),
                 ),
               ),
-                  
-                  
-                  ),
             ],
           ),
         ),
