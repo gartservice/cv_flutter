e137203a36a7bacf39be1f193313601c8628e575
gartservice <gart.service@yandex.ru>
2023-01-15 09:06:38 +0800

    Container for mobile and email.

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,94 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Column(
             children: [
-              Column(
+              CircleAvatar(
+                maxRadius: 100.0,
+                backgroundImage: AssetImage('images/avatar.jpg'),
+              ),
+              SizedBox(height: 15),
+              Text(
+
+                'Artem Garanskyy',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontFamily: 'BebasNeue',
+                  color: Colors.white,
+                  letterSpacing: 2.5
+                ),
+              ),
+              Text(
+                'Flutter developer',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'SourceCodePro',
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                  letterSpacing: 5,
+                ),
+              ),
+              Container(
+                  padding: EdgeInsets.all(10.0),
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        size: 25,
+                        color: Colors.teal.shade900,
+                      ),
+                      SizedBox(
+                        width: 12,
+                      ),
+                      Text(
+                        '+61 415 555 555',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'PlayfairDisplay',
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  )),
+              Container(
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                color: Colors.white,
+              child: Row(
+
                 children: [
-                  SizedBox(
-                    height: 150.0,
-                  ),
-                  Container(
-                    child: CircleAvatar(
-                      maxRadius: 100.0,
-                      backgroundImage: AssetImage('images/avatar.jpg'),
+                  Icon(Icons.mail,size: 20, color: Colors.teal.shade900),
+                  SizedBox(width: 12),
+                  Text('artem.dev@gmail.com',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'PlayfairDisplay',
+                      color: Colors.teal.shade900,
                     ),
-                  ),
-                  Container(
-                    child: Text(
-                      'Artem Garanskyy',
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontFamily: 'BebasNeue',
-                        color: Colors.black87,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    child: Text(
-                      'Flutter developer',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: 'SourceCodePro',
-                        color: Colors.black54,
-                        fontWeight: FontWeight.w900,
-                        letterSpacing: 5,
-
-                      ),
-                    ),
-                  ),
-
-                  Container(
-                    child: Text('text'),
-                    width: 400,
-                    height: 100.0,
-                    color: Colors.white,
-
                   ),
                 ],
-              )
+              ),
+                  
+                  
+                  ),
             ],
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
